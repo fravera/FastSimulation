c87e8a6e6507a183c4520e55d4c86feeba15396c
fravera <fabio.ravera89@gmail.com>
2016-05-18 20:18:30 +0200

    Corrected PPSToFDetector

--- PPSFastSim/src/PPSToFDetector.cc
+++ PPSFastSim/src/PPSToFDetector.cc
@@ -4,16 +4,16 @@
 //=====================================================================================================
 
 PPSToFDetector::PPSToFDetector(double detWidth, double detHeight, double detPosition):
-        DetW(detHeight),DetH(detHeight),DetPosition(detPosition){
+        DetWidth(detHeight),DetHeight(detHeight),DetPosition(detPosition){
 };
 
 //--------------------------------------------------------------------------------------------------------------//
 
 void PPSToFDetector::AddHit(double x, double y, double tof) {
    if (x>0) return; // The detector is on the negative side
-   if (fabs(x)>DetectorWidth+DetectorPosition) return; // hit beyond detector area (W)
-   if (fabs(x)<DetectorPosition) return;               // hit falls below detector area
-   if (fabs(y)>DetectorHeight) return;                 // hit falls beyond detector area (H)
+   if (fabs(x)>DetectorWidth+DetPosition) return; // hit beyond detector area (W)
+   if (fabs(x)<DetPosition) return;               // hit falls below detector area
+   if (fabs(y)>DetHeight) return;                 // hit falls beyond detector area (H)
    X.push_back(x);
    Y.push_back(0.);
    ToF.push_back(tof);
